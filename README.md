40d4e986b49fea0dc7659864bf70c4c97473f2aa
undefined <mayun@taobao.com>
2020-08-26 00:06:36 +0800

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nameless Goland Plugin
+
+## TODO
+
+- generate proto message from model struct
+
+- git flow
+
+- generate gorm model struct from database
+
+- 
---
+++ src/com/cyrivlclth/goplugins/demo/DemoAction.java
@@ -0,0 +1,15 @@
+package com.cyrivlclth.goplugins.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author cyrivl
+ */
+public class DemoAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
